50a96b0f220ed28474c9981a17c448395e974cb8
beklc-sys <christoph.beklas@gmail.com>
2026-07-29 22:46:25 +0200

    Cache modular FairWash files

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const CACHE_NAME = "fairwash-v7";
+const CACHE_NAME = "fairwash-v8";
 const APP_FILES = [
   "./",
   "./index.html",
+  "./style.css",
+  "./app.js",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
@@ -12,9 +14,11 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(
-    keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-  )));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
   self.clients.claim();
 });
 
@@ -22,8 +26,10 @@ self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(
     fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      if (response.ok && event.request.url.startsWith(self.location.origin)) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      }
       return response;
     }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
   );
